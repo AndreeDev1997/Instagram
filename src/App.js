7f8b35c38c9fc724cf34b34a4ec52e1e0ea903be
Richard <andreerb.2020@gmail.com>
2020-05-21 17:50:19 -0500

    App.js modified

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.css";
-
-import UserLogin from "./pages/UserLogin/UserLogin";
 import UserRegister from "./pages/UserRegister/UserRegister";
 
 function App() {
-  return <UserRegister/>;
+  return <UserRegister />;
 }
 export default App;
