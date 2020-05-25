5ecb9a23a798c1a04bc5ffb5eda2793f15338fb4
Richard <andreerb.2020@gmail.com>
2020-05-25 18:02:53 -0500

    Login added

--- src/pages/UserLogin/UserLogin.js
+++ src/pages/UserLogin/UserLogin.js
@@ -13,7 +13,39 @@ import UserRegister from "../UserRegister/UserRegister";
 const UserLogin = () => {
   return (
     <div className="main-wrapper">
-      <UserRegister />
+      {/* <UserRegister /> */}
+      <div className="wrapper-containerMain wrapper-container-l">
+        <Span />
+      </div>
+      <div className="wrapper-containerMain wrapper-container-r">
+        <div className="wrapper-userLogin-top ">
+          <TextMain isDisplayDescription={false} text={""} />
+          <LoginForm className="loginForm" />
+        </div>
+        <div className="wrapper-little-container ">
+          ¿No tienes una cuenta?
+          <a href="algo" className="ml-1 links">
+            Registrate
+          </a>
+        </div>
+        <div className="wrapper-little-container column">
+          <div className="wrapper-download">Descarga App</div>
+          <div className="wrapper-buttonApp">
+            <DownloadButton
+              link="https://apps.apple.com/us/app/instagram/id389801252"
+              img={imgAppStore}
+              information="Descárgalo en"
+              app={"App Store"}
+            />
+            <DownloadButton
+              link="https://play.google.com/store/apps/details?id=com.instagram.android&hl=en"
+              img={imgPlayStore}
+              information="DISPONIBLE EN"
+              app={"Google Pay"}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
