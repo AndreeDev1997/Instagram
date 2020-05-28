437d9adb8b82d32ca9bb19af13a0305abb6b689b
Fiorella <mayte069806@gmail.com>
2020-05-28 16:36:02 -0500

    react-icons added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import UserLogin from "./pages/UserLogin/UserLogin";
 import UserRegister from "./pages/UserRegister/UserRegister";
+import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   return (
     <div>
-       {/* <UserRegister />  */}
-        <UserLogin />    
+        <Navigation></Navigation>
     </div>
   );
 }
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { FaAngry} from 'react-icons/fa';
+import './Navigation.css'
+
+const Navigation = () => {
+    return ( 
+    <FaAngry className="a"/>
+    );
+}
+ 
+export default Navigation;
